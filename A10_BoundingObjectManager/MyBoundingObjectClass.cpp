75e70e6b5a7c7b80134737e3bc535af827048e6e
Henry Zhang (RIT Student) <hxz3509@ad.rit.edu>
2016-04-20 20:46:03 -0400

    Parts e-h

--- A10_BoundingObjectManager/MyBoundingObjectClass.cpp
+++ A10_BoundingObjectManager/MyBoundingObjectClass.cpp
@@ -31,3 +31,104 @@ vector3 MyBoundingObjectClass::GetMax()
 {
     return max;
 }
+
+//Get and Set the "Model to World" matrix of the BO
+matrix4 MyBoundingObjectClass::GetSetWorld(matrix4 arg_World)
+{
+	worldMatrix = arg_World;
+	//recalc min/max
+	for (uint i = 0; i < points.size(); i++)
+	{
+		vector3 point = vector3(worldMatrix * vector4(points[i], 1.0f));
+
+		if (i == 0)
+			min = max = vector3(point.x, point.y, point.z);
+
+		if (point.x > max.x)
+			max.x = point.x;
+		else if (point.x < min.x)
+			min.x = point.x;
+
+		if (point.y > max.y)
+			max.y = point.y;
+		else if (point.y < min.y)
+			min.y = point.y;
+
+		if (point.z > max.z)
+			max.z = point.z;
+		else if (point.z < min.z)
+			min.z = point.z;
+	}
+
+	float tmp;
+
+	//fix issue where max goes under min and vice versa
+	if (max.x < min.x)
+	{
+		tmp = min.x;
+		min.x = max.x;
+		max.x = tmp;
+	}
+	if (max.y < min.y)
+	{
+		tmp = min.y;
+		min.y = max.y;
+		max.y = tmp;
+	}
+	if (max.z < min.z)
+	{
+		tmp = min.z;
+		min.z = max.z;
+		max.z = tmp;
+	}
+
+	center = (max + min) / 2.0f;
+	//m_fRadius = glm::distance(m_v3Center, max);
+	size.x = glm::distance(vector3(min.x, 0.0, 0.0), vector3(max.x, 0.0, 0.0));
+	size.y = glm::distance(vector3(0.0, min.y, 0.0), vector3(0.0, max.y, 0.0));
+	size.z = glm::distance(vector3(0.0f, 0.0, min.z), vector3(0.0, 0.0, max.z));
+
+	return worldMatrix;
+}
+
+//sets color of the BO
+void MyBoundingObjectClass::SetColor(vector3 arg_Color)
+{
+	color = arg_Color;
+}
+
+void MyBoundingObjectClass::Draw()
+{
+	m_pMeshMngr->AddCubeToQueue(
+		glm::translate(center) *
+		glm::scale(size), color, WIRE);
+}
+
+bool MyBoundingObjectClass::CheckCollision(MyBoundingObjectClass* const a_pOther)
+{
+	bool bAreColliding = true;
+	vector3 vMin1 = vector3(worldMatrix * vector4(min, 1.0f));
+	vector3 vMax1 = vector3(worldMatrix * vector4(max, 1.0f));
+	vector3 vMin2 = vector3(a_pOther->worldMatrix * vector4(a_pOther->min, 1.0f));
+	vector3 vMax2 = vector3(a_pOther->worldMatrix * vector4(a_pOther->max, 1.0f));
+
+	//Check for X
+	if (vMax1.x < vMin2.x)
+		bAreColliding = false;
+	if (vMin1.x > vMax2.x)
+		bAreColliding = false;
+
+	//Check for Y
+	if (vMax1.y < vMin2.y)
+		bAreColliding = false;
+	if (vMin1.y > vMax2.y)
+		bAreColliding = false;
+
+	//Check for Z
+	if (vMax1.z < vMin2.z)
+		bAreColliding = false;
+	if (vMin1.z > vMax2.z)
+		bAreColliding = false;
+
+	return bAreColliding;
+}
--- A10_BoundingObjectManager/MyBoundingObjectClass.h
+++ A10_BoundingObjectManager/MyBoundingObjectClass.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <vector>
 #include "RE\ReEng.h"
-class MyBoundingObjectClass
+#include "RE\ReEngAppClass.h"
+
+using namespace ReEng;
+
+class MyBoundingObjectClass : public ReEngAppClass
 {
 public:
     MyBoundingObjectClass(std::vector<vector3>);
@@ -11,11 +15,22 @@ public:
     vector3 GetCenter();
     vector3 GetMin();
     vector3 GetMax();
+
+	matrix4 GetSetWorld(matrix4);
+	void SetColor(vector3);
+	void Draw();
+	bool CheckCollision(MyBoundingObjectClass* const a_pOther);
+	typedef ReEngAppClass super;
 private:
     std::vector<vector3> points;
     bool visible;
+
     vector3 center;
     vector3 min;
     vector3 max;
+	matrix4 worldMatrix;
+	vector3 color;
+	vector3 size;
+
 };
 
